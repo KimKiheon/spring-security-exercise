3fa102d9e5b3c8385cbd4174a5209385cfed3e02
Kimkiheon <cbj2741@gmail.com>
2022-11-29 13:02:06 +0900

    ADD: UserController

---
+++ src/main/java/com/example/springsecurityexercise/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.springsecurityexercise.controller;
+
+import com.example.springsecurityexercise.domain.Response;
+import com.example.springsecurityexercise.domain.dto.UserDto;
+import com.example.springsecurityexercise.domain.dto.UserJoinRequest;
+import com.example.springsecurityexercise.domain.dto.UserJoinResponse;
+import com.example.springsecurityexercise.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor //final이나 notnull 생성자 주입
+public class UserController {
+    private final UserService userService;
+    @PostMapping
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest){
+        UserDto userDto = userService.join(userJoinRequest);
+        return Response.success(new UserJoinResponse(userDto.getUserName(),userDto.getEmailAddress()));
+    }
+}
