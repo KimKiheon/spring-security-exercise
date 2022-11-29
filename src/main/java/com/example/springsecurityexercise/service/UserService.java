17feb868a8d12122b9f319b767875b37c42db322
Kimkiheon <cbj2741@gmail.com>
2022-11-29 11:45:10 +0900

    ADD: UserService, join

---
+++ src/main/java/com/example/springsecurityexercise/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.springsecurityexercise.service;
+
+import com.example.springsecurityexercise.domain.dto.UserDto;
+import com.example.springsecurityexercise.domain.dto.UserJoinRequest;
+import com.example.springsecurityexercise.domain.entity.User;
+import com.example.springsecurityexercise.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    public UserDto join(UserJoinRequest request){
+        User savedUser = userRepository.save(request.toEntity());
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .emailAddress(savedUser.getEmailAddress())
+                .build();
+    }
+}
