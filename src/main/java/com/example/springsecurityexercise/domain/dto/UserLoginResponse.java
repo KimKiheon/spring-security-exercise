c09eb0b12fad9d55d611f3b2399108ae7bfb248e
Kimkiheon <cbj2741@gmail.com>
2022-11-30 10:16:29 +0900

    ADD: UserController -> Login Mapping

--- src/main/java/com/example/springsecurityexercise/domain/dto/UserLoginResponse.java
+++ src/main/java/com/example/springsecurityexercise/domain/dto/UserLoginResponse.java
@@ -1,5 +1,10 @@
 package com.example.springsecurityexercise.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class UserLoginResponse {
     private String token;
 }
