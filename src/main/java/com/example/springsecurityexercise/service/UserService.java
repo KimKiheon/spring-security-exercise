8bf1e24cd2f84237eadd7942f8502694b00b5f42
Kimkiheon <cbj2741@gmail.com>
2022-11-30 10:14:40 +0900

    ADD: UserService -> Login 기능

--- src/main/java/com/example/springsecurityexercise/service/UserService.java
+++ src/main/java/com/example/springsecurityexercise/service/UserService.java
@@ -3,8 +3,12 @@ package com.example.springsecurityexercise.service;
 import com.example.springsecurityexercise.domain.dto.UserDto;
 import com.example.springsecurityexercise.domain.dto.UserJoinRequest;
 import com.example.springsecurityexercise.domain.entity.User;
+import com.example.springsecurityexercise.exception.ErrorCode;
+import com.example.springsecurityexercise.exception.HospitalReviewAppException;
 import com.example.springsecurityexercise.repository.UserRepository;
+import com.example.springsecurityexercise.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +17,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
-    public UserDto join(UserJoinRequest request){
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
+    public UserDto join(UserJoinRequest request) {
         userRepository.findByUserName(request.getUserName())
-                .ifPresent(user ->{
+                .ifPresent(user -> {
                     throw new RuntimeException("UserName 중복");
                 });
         User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
@@ -24,5 +31,16 @@ public class UserService {
                 .userName(savedUser.getUserName())
                 .email(savedUser.getEmailAddress())
                 .build();
+    }
+
+    public String login(String userName, String password) {
+        User user = userRepository.findByUserName(userName).orElseThrow(
+                () -> new HospitalReviewAppException(ErrorCode.NOT_FOUND,
+                        String.format("%s는 가입되지 않은 userName입니다")));
+        if (!encoder.matches(password, user.getPassword())) {
+            throw new HospitalReviewAppException(ErrorCode.INVALID_PASSWORD, "password가 올바르지 않습니다");
+        }
+        return JwtTokenUtil.createToken(userName, secretKey, 1000 * 60 * 60);
+
     }
 }
